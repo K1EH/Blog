e1c565a6de8c7d065c155ec73bde1e6b15d180b9
Jitae Choi <jitae01@gachon.ac.kr>
2025-10-16 04:29:36 +0900

    chore: personalize blog and add env

--- site.config.js
+++ site.config.js
@@ -1,32 +1,40 @@
+/*
+git add .
+git commit -m "chore: update site config and domain"
+git push
+*/
+
 const CONFIG = {
-  // profile setting (required)
+  // ===============================
+  // 👤 Profile (required)
+  // ===============================
   profile: {
-    name: "morethanmin",
+    name: "Jitae Choi",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "AI Developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "jitae.choi01@gmail.com",
+    linkedin: "",
+    github: "K1EH",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `name`,
+      href: "",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Jitae'sBlog",
+    description: "Dev Blog",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "jitae-dev.vercel.app",
+  since: 2025, // If leave this empty, current year will be used.
+  lang: "ko_KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
